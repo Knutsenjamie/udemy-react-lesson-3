0cda3b228cb9131a59e2d2dee67bbe002f855244
“Jamie <knutsenjamie@yahoo.com>
2021-08-06 11:32:27 -0700

    write geolocation method in app as Functional Component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>WELL HELLO THERE</div>
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    );
+
+    return <div>Latitude: </div>
 };
 
 ReactDOM.render(
     <App/>, 
-    document.querySelector('#root')
-);
+    document.querySelector('#root'));
