e50da0e319e32305ad09c1b554f2a8c147a18485
“Jamie <knutsenjamie@yahoo.com>
2021-08-13 09:55:41 -0700

    add constructor with comments on initializing state

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+
+//React requires you to define render()
 class App extends React.Component {
+    contructor(props){
+        //you always need to call super(props); when doing the constructor to initilize state
+        super(props);
+        // lat is latitude. null because we don't know what the lat is YET
+        this.state = { lat: null };
+    }
+
+
+
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
