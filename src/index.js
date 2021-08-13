4a59c15d1551aca6568c74bd4831296efd1a0a54
“Jamie <knutsenjamie@yahoo.com>
2021-08-13 10:15:39 -0700

    fetch actual location using setState and use comments to explain

--- src/index.js
+++ src/index.js
@@ -8,18 +8,23 @@ class App extends React.Component {
     constructor(props){
         super(props);
        
+        // this is the ONLY TIME we do direct assignment of this.state... since we are initializing it!
         this.state = { lat: null };
+        // move this outside the render method so that you don't end up double-fetching location. This is why it is important to leave state changes outside render method. Double fetching will cause things to slow down otherwise. 
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            // ^ above we are updating state (so have to use setState); and calling the coordinates of our current locations latitude (coords and latitiude are built in with the geolocation function)
+
+            // we did not do this.state.lat = position.coords.latitude. YOU NEVER WANT TO do a direct assignment to your state object!!! The exception is when we initialize our state in our constructor function. 
+            (err) => console.log(err)
+        );
     }
 
 
 
     render() {
-        // never want to initialize state within render method. It will get called too frequently otherwise.
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
-
         return <div>Lattitude: {this.state.lat}</div>;
     }
 }
